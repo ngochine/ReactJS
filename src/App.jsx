908f075e47f78b81556127a8db1ed82430e3fe31
ngocTrinhHo <2351050189trinh@ou.edu.vn>
2025-04-05 22:11:31 +0700

    add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,189 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useMemo, useEffect, useRef } from "react";
+import { Button, Input, Pagination } from "antd";
+import {
+  CheckCircleTwoTone,
+  DeleteOutlined,
+  SortAscendingOutlined,
+  SortDescendingOutlined,
+} from "@ant-design/icons";
 import "./App.css";
-import { Button } from 'antd';
-import { Input } from 'antd';
-import { Checkbox } from "antd";
-import { DatePicker } from "antd";
 
-// import dayjs from "dayjs";
-
-/*
-id: string
-content: string
-isDone: boolean
+/* Todo interface
+  - id: number
+  - todo: string
+  - completed: boolean
+  - createdAt: string
 */
 
 function App() {
-  const now= new Date()
-  const [presentDate, setPresentDate] = useState(`${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`)
-  // const [todos, setTodos] = useState([
-  //   { id: 1, content: "hehe", isDone: false, ngayTao: presentDate },
-  //   { id: 2, content: "haha", isDone: false, ngayTao: presentDate },
-  // ]);
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")))
-  localStorage.setItem("todos", JSON.stringify(todos))
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [sortDirection, setSortDirection] = useState("asc");
+  "asc" || "desc";
+  const [pagination, setPagination] = useState({limit: 10, skip: 0});
+  const [total, setTotal] = useState(0)
 
-  // const todoss = JSON.parse(localStorage.getItem("todos"))
-  // setTodos(todoss)
-  const [newTodo, setNewTodo] = useState('');
-  const [thongBao, setThongBao] = useState('');
-  const [status, setStatus] = useState(false)
-  const [arrReset, setArrReset] = useState(todos)
+  useEffect(()=>{
+    // console.log(pagination)
+    fetch('https://dummyjson.com/todos?limit=' + pagination.limit + '&skip=' + pagination.skip)
+    .then(res => res.json())
+    .then(data => {
+      setTodos(data.todos)
+      setTotal(data.total)
+    });
+  }, [pagination])
 
-  function addTodo() {
+  function handleAddTodo() {
     const newTodos = [...todos];
-    const todoMaxId= todos.reduce((a,b)=>b.id > a.id?b:a,{id:0})
-    setPresentDate(`${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`)
-    // newTodos.push({ id: todos[todos.length-1].id +1 , content: newTodo, isDone: false });
-    newTodos.push({ id: todoMaxId.id +1 , content: newTodo, isDone: false, ngayTao: presentDate });
+    const maxId = todos.reduce(
+      (maxId, currentTodo) => (currentTodo.id > maxId ? currentTodo.id : maxId),
+      0,
+    );
+
+    newTodos.push({
+      id: maxId + 1,
+      todo: newTodo,
+      completed: false,
+      createdAt: new Date().toLocaleString(),
+    });
+
     setTodos(newTodos);
-    !todos.length ? setThongBao("") :
-    setNewTodo('');
-    setArrReset(newTodos)
-    // setTodos(todoss)
-  }
-  
-  function handleInputChange(event){
-    setNewTodo(event.target.value)
+    setNewTodo("");
   }
 
-  function deleteTodo(id){
-    const newTodos = todos.filter(todo=>
-      todo.id!==id
+  function handleInputChange(event) {
+    setNewTodo(event.target.value);
+  }
+
+  function handleDeleteTodo(id) {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  function handleCheckboxChange(id) {
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo,
     );
     setTodos(newTodos);
+    setNewTodo("");
   }
 
-  function handleCheckboxChange(id){
-    const newTodos = todos.map(todo=>
-        todo.id===id ? {...todo, isDone: !todo.isDone} : todo
-    );
-    setTodos(newTodos)
+  const sortedTodos = useMemo(() => {
+    return [...todos].sort((todo1, todo2) => {
+      if (sortDirection === "asc") {
+        return new Date(todo1.createdAt) - new Date(todo2.createdAt);
+      } else {
+        return new Date(todo2.createdAt) - new Date(todo1.createdAt);
+      }
+    });
+  }, [todos, sortDirection]);
+
+  function handleSort() {
+    switch (sortDirection) {
+      case "asc":
+        setSortDirection("desc");
+        break;
+      case "desc":
+        setSortDirection("asc");
+        break;
+      default:
+        break;
+    }
+  }
+
+  function handleDeleteAll() {
+    setTodos([]);
   }
   
-  function handleSortTodo(status){
-    const newTodos= [...todos].sort((todo1, todo2)=>
-      status? (todo1.content.length - todo2.content.length): - (todo1.content.length - todo2.content.length)
-    )
-    setStatus(!status)
-    setTodos(newTodos)
+  function handlePaginationChange(page, pageSize){
+    setPagination({
+      ...pagination ,skip: (page-1) * pageSize 
+    })
   }
 
-  function handleDeleteAll(){
-    !todos.length? setThongBao("Không còn gì để xoá") : 
-    setTodos([])
-  }
-  function handleReset(){
-    setTodos(arrReset)
+  function handleShowSizeChange(_, size){
+    setPagination(p=>({
+      limit: size, skip:0
+    }))
+    console.log(size)
   }
 
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold">TODO LIST</h1>
-      <p>{thongBao}</p>
-      <div className="w-[70%] m-auto p-4 border border-solid border-regal-blue rounded-lg">
-        {todos.map((todo, index) => (
-          <div className="m-4 p-4 flex justify-between rounded-lg">
-            <span className="text-[12px]">{todo.ngayTao}</span>
-            <Checkbox onChange={()=> handleCheckboxChange(todo.id)}/>
-            <span>
-              {todo.id}. {todo.content} {todo.isDone ? "(Đã done)":''}
-            </span>
-            <Button className="btnDelete" onClick={()=> deleteTodo(todo.id)}>DELETE</Button>
-          </div>
-        ))}
+    <div className="flex flex-col items-center mt-8 gap-8">
+      <h1 className="text-3xl font-bold">Todo List</h1>
+      <div className="w-[70%] m-auto p-4 border border-neutral-300 shadow-md rounded-lg">
+        {sortedTodos.length ? (
+          sortedTodos.map((todo, idx) => (
+            <div
+              key={todo.id}
+              className="m-4 p-4 flex justify-between items-center"
+            >
+              <div className="flex">
+                <p>{todo.id}.</p>
+                <div className="ml-2">
+                  <p className={`${todo.completed && "line-through"}`}>
+                    {todo.todo}
+                    {todo.completed && (
+                      <CheckCircleTwoTone
+                        className="ml-2"
+                        twoToneColor="#299B43"
+                      />
+                    )}
+                  </p>
+                  <p className="text-[12px] text-neutral-500 italic">
+                    {todo.createdAt}
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex gap-2">
+                <Button onClick={() => handleCheckboxChange(todo.id)}>
+                  {todo.completed ? "Mark as Undone" : "Mark as Done"}
+                </Button>
+                <Button danger onClick={() => handleDeleteTodo(todo.id)}>
+                  <DeleteOutlined />
+                </Button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center my-4">No todo left. Well done!</p>
+        )}
         <div className="flex justify-center gap-4">
           <Input value={newTodo} type="text" onChange={handleInputChange} />
-          <Button disabled={!newTodo.length} onClick={addTodo}>Add</Button>
+          <Button disabled={!newTodo.length} onClick={handleAddTodo}>
+            Add
+          </Button>
         </div>
         <div className="text-center mt-8 flex justify-center gap-4">
-          <Button onClick={()=>handleSortTodo(status)}>Sắp xếp</Button>
-          <Button onClick={handleDeleteAll}>Xoá tất cả</Button>
-          <Button onClick={handleReset}>RESET</Button>
+          <Button
+            icon={
+              sortDirection === "asc" ? (
+                <SortDescendingOutlined />
+              ) : (
+                <SortAscendingOutlined />
+              )
+            }
+            onClick={handleSort}
+          >
+            Sort
+          </Button>
+          <Button danger onClick={handleDeleteAll}>
+            Delete All
+          </Button>
+        </div>
+        <div className="flex justify-center mt-8">
+            <Pagination
+              pageSize={pagination.limit}
+              current={(pagination.skip / pagination.limit) +1}
+              
+              total={total}
+              pageSizeOptions={[10, 20, 50, 100]}
+              onChange={handlePaginationChange}
+              onShowSizeChange={handleShowSizeChange}
+            ></Pagination>
         </div>
       </div>
-
     </div>
   );
 }
